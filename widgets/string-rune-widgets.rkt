1668cf0ea983afc0912251aa88fa82e9d6690f5a
unknown <stephen@thoughtstem.com>
2020-10-16 14:10:48 -0700

    modal editor

--- widgets/string-rune-widgets.rkt
+++ widgets/string-rune-widgets.rkt
@@ -1,10 +1,13 @@
 #lang at-exp racket
 
 (provide string-rune-widget
-         raw-code-rune-widget)
+         (rename-out [raw-code-rune-widget modal-editor-rune-widget]) ;Use this name instead of raw-code-rune-widget now
+         raw-code-rune-widget
+         )
 
 (require website-js
          (only-in webapp/js late-include-js)
+         
 	 codespells-runes/rune-util
          codespells-runes/widgets/util
          (only-in codespells-runes/rune-description-lang svg-rune-description rune-background)         )
@@ -78,50 +81,80 @@
 
  }))))
 
-(define (raw-code-rune-widget)
-  (local-require website/svg)
+
+(require website/svg)
+
+;One for editing "raw code" in a CodeMirror text area,
+;  now updated to be more general:
+;Manages a component shown in a model, and a Rune.
+;  Click the Rune and see the component.
+;Use the js snippet params to customize how the state of the widget component
+; is reflected in the Rune component.
+;For extra clean code -- make both the widget component and the Rune be enclosures,
+;  make the js snippets just call-method on them.
+(define (raw-code-rune-widget
+         #:initial-rune [initial-rune (svg-rune-description
+                                       (rune-background
+                                        #:color "gray"
+                                        (text
+                                         class: "displayString"
+                                         fill: "gray"
+                                         x: "40%" ;Not sure why this isn't working at 50%.  Border?
+                                         y: "40%"
+                                         'text-anchor: "middle"
+                                         'dominant-baseline: "middle"
+                                         (~a "()"))))]
+         #:editor [inside-modal-component (editor-component "()")]
+         #:on-show-editor [on-show-js (lambda (component)
+                                 (call-method component 'refresh))]
+         #:compile-editor [on-compile-js (lambda (component)
+                                       (call-method component 'getString))]
+
+         #:update-rune-preview [update-rune-js
+                        (lambda (rune-component val)
+                          ;TODO: make this a method on the Rune that controls the CodeMirror
+                          @js{
+                          $(@rune-component).find(".displayString").html("\""+ @val .substring(0,3)+"...\"");
+                         })])
+  
   
   (enclose
    (div id: (id 'id) 'data-id: "(list)"
         data-toggle: "modal" data-target: (~a "#" (id 'modal))
-        (svg-rune-description
-         (rune-background
-          #:color "gray"
-          (text
-           class: "displayString"
-           fill: "gray"
-           x: "40%" ;Not sure why this isn't working at 50%.  Border?
-           y: "40%"
-           'text-anchor: "middle"
-           'dominant-baseline: "middle"
-           (~a "()")))))
+        initial-rune
+        )
    (script ([construct (call 'constructor)])
            (function (constructor)
                      @js{
-                       @(register-modal-widget (raw-code-widget-modal))
+                       @(register-modal-widget
+                         (raw-code-widget-modal
+                          #:inside-modal-component inside-modal-component))
                        $(@(~j "#NAMESPACE_modal")).on("shown.bs.modal",()=>@(call 'onShowModal))
                      })
            (function (onShowModal)
                      ;Gotta call refresh on the editor because CodeMirror needs to do a relayout
                      @js{
-                         var editor = document.querySelector(@(~j "#NAMESPACE_modal .editorComponent"));
-                         @(call-method 'editor 'refresh) })
+                         var editor = document.querySelector(@(~j "#NAMESPACE_modalBody > :first-of-type"));
+                         @(on-show-js 'editor) })
            (function (done)
                      @js{
-                       var editor = document.querySelector(@(~j "#NAMESPACE_modal .editorComponent"));
-                       var val = @(call-method 'editor 'getString)
+                       var editor = document.querySelector(@(~j "#NAMESPACE_modalBody > :first-of-type"));
+                       var val = @(on-compile-js 'editor)
 
                        $(@(~j "#NAMESPACE_id")).attr("data-id", val)
 
-                       $(@(~j "#NAMESPACE_id .displayString")).html("\""+val.substring(0,3)+"...\"")
+                       var rune = document.querySelector(@(~j "#NAMESPACE_id > :first-of-type"));
+
+                       @(update-rune-js 'rune 'val)
                      }))))
 
-(define (raw-code-widget-modal)
+(define (raw-code-widget-modal
+         #:inside-modal-component [inside-modal-component (editor-component "()")])
   (modal id: (id 'modal) role: "dialog"
-         (modal-dialog
+         (modal-dialog class: "modal-lg" style: "max-width: 80% !important"
           (modal-content
-           (modal-body
-            (editor-component "()")  
+           (modal-body id: (id 'modalBody)
+            inside-modal-component  
             (modal-footer
              (button-primary
               'data-dismiss: "modal"
