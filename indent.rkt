883223b15e36637213993c4616344fa13912eb0d
Stephen Foster <stephen@thoughtstem.com>
2020-11-13 13:38:42 -0800

    disabling tabbify cuz sloooow

--- indent.rkt
+++ indent.rkt
@@ -29,26 +29,59 @@
 		[(_ p)
 		 #`(->skeleton-f #'p) ]))
 
-	(provide tabify)
+(provide tabify)
 
 
 (require racket/runtime-path)
 (define-runtime-path me "indent.rkt")
 (define (tabify s)
   (displayln s)
+
+  ;Turns out that simply not tabifying it is best 
+  s
+
+  ;Possible other algorithm:
+  ;  current-removed-space is 0
+  ;  Go line by line, 
+  ;    remove current-removed-space leading spaces 
+  ;    compress the rest into 1 space, add the amount compressed to current-removed-space
+
+
+  ;FAILED TRY 1: This creates unnatural breaks no matter what you set 
+  ; the second arg (columns) to. E.g. (define x 1) becomes
+  ; (define
+  ;  x
+  ;  1)
+  ;Plus, the extra lines added make the height calculation wrong on typeset-runes-block
+  #;
+  (pretty-format (read (open-input-string s))
+		 1)
+
+
+  ;FAILED TRY 2:
+  ;  This "works" but the workaround for the racket/gui/base bug requires 
+  ;  having this file run itself in another process (by calling itself on the
+  ;  command line).  It is so SLOOOOW that it becomes unbearable 
+  ;  to build codespells.org
+
   ;Such a dumb hack for the racket/gui/base bs...  
   ; https://www.mail-archive.com/racket-users@googlegroups.com/msg40123.html
+  #;
   (define temp (make-temporary-file))
+
+  #;
   (with-output-to-file 
     #:exists 'replace
     temp 
     (thunk (display s)))
 
+  #;
   (read
     (open-input-string
       (with-output-to-string
 	(thunk
-	  (system (~a "racket " me " " temp)))))))
+	  (system (~a "racket " me " " temp))))))
+  )
 
 (module+ main
 	 (define args
